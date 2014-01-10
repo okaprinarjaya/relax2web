083b587f44b20366bc9e3c6eac27d9fc69cad2f4
Oka Prinarjaya <okaprinarjaya@gmail.com>
2014-01-10 16:55:03 +0700

    directly returning array instead of storing it to a variable. Then add some new configuration properties

--- src/config.php
+++ src/config.php
@@ -1,13 +1,17 @@
 <?php
-define('DS', DIRECTORY_SEPARATOR);
-
-function getAppConfig()
-{
-    $config = array();
-	$config['base_url'] = 'http://localhost:8000/';
-	$config['common_js'] = array('jquery','bootstrap.min');
-	$config['common_js_additional_vars'] = array('base_url' => $config['base_url']);
-	$config['upload_dir'] = '/var/www/html/uploads';
-	
-	return $config;
+function getAppConfig() {
+	return array(
+		'base_url' => 'http://localhost:8000/',
+		'template' => 'Default',
+		'modules_dir' => 'src/Module/',
+		'templates_dir' => 'src/Template/',
+		'common_js' => array(
+			'jquery',
+			'bootstrap.min'
+		),
+		'common_js_additional_vars' => array(
+			'base_url' => 'http://localhost:8000/'
+			),
+		'upload_dir' => '/path/to/upload/dir',
+	);
 }
