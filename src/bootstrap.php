c343407c5ae4bdcca4fddc3cf56bb32168e4d483
Oka Prinarjaya <okaprinarjaya@gmail.com>
2014-01-11 11:33:04 +0700

    Adding bootstrapping, add new item to ignore, bootstrapping inindex.php

---
+++ src/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+require realpath(__DIR__ . '/../vendor/autoload.php');
+require realpath(__DIR__ . '/config.php');
+
+date_default_timezone_set('Asia/Jakarta');
+Propel::init("propelconf/connection.php");
+
+$app = new \Slim\Slim(array(
+	'view' => new \src\Common\AppTwigViewBase()
+));
+
+$app->app_conf = getAppConfig();
+$view = $app->view();
+
+$view->parserOptions = array(
+	'debug' => true
+);
+
+$view->parserExtensions = array(
+	new \Slim\Views\TwigExtension()
+);
+
+$view->setTemplatesDirectory($app->app_conf['templates_dir'].$app->app_conf['template'].'/');
+
+$app->add(new \src\Common\AppSlimMiddleware());
+
+/**
+ * 
+ * Start to set default routes.
+ * 
+ */
+$app->get('/', function () use ($app) {
+	$app->render('modules/main/index.html');
+});
+
+$app->run();
